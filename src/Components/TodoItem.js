6fab41f861193dff595f473b8b17b9fc8d3cf9bf
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-17 13:38:41 +0200

    Changed TodoItem component to a functional component

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
-class TodoItem extends Component {
-  completeTodo(todo) {
-    this.props.onCompleted(todo);
-  }
-  deleteTodo(todo) {
-    this.props.onDeleted(todo);
-  }
-  render() {
-    let todoClass = this.props.todo.completed ? 'done' : 'undone';
-    return (
-      <div className={todoClass}>
-        <p>
-          {this.props.todo.title}
-        </p>
-        <button
-          className="completed"
-          onClick={() => this.completeTodo(this.props.todo)}
-        >
-          Completed
-        </button>
-        <button onClick={() => this.deleteTodo(this.props.todo)}>X</button>
-      </div>
-    );
-  }
-}
+const TodoItem = props => {
+  let todoClass = props.todo.completed ? 'done' : 'undone';
+  return (
+    <div className={todoClass}>
+      <p>
+        {props.todo.title}
+      </p>
+      <button
+        className="completed"
+        onClick={() => props.onCompleted(props.todo)}
+      >
+        Completed
+      </button>
+      <button onClick={() => props.onDeleted(props.todo)}>X</button>
+    </div>
+  );
+};
 
 TodoItem.propTypes = {
-  todo: PropTypes.object
+  todo: PropTypes.object,
+  onCompleted: PropTypes.func,
+  onDeleted: PropTypes.func
 };
 
 export default TodoItem;
