e5e9fa77f711e34ec43aa2b451cb7b65d22062c4
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-17 13:43:44 +0200

    Changed Todos component to a functional component

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -2,37 +2,31 @@ import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem'; // eslint-disable-line no-unused-vars
 
-class Todos extends Component {
-  completeTodo(todo) {
-    this.props.onCompleted(todo);
+const Todos = props => {
+  let todoItems;
+  if (props.todos) {
+    todoItems = props.todos.map(todo => {
+      return (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          onCompleted={() => props.onCompleted(todo)}
+          onDeleted={() => props.onDeleted(todo)}
+        />
+      );
+    });
   }
-  deleteTodo(todo) {
-    this.props.onDeleted(todo);
-  }
-  render() {
-    let todoItems;
-    if (this.props.todos) {
-      todoItems = this.props.todos.map(todo => {
-        return (
-          <TodoItem
-            todo={todo}
-            key={todo.id}
-            onCompleted={() => this.completeTodo(todo)}
-            onDeleted={() => this.deleteTodo(todo)}
-          />
-        );
-      });
-    }
-    return (
-      <div>
-        {todoItems}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {todoItems}
+    </div>
+  );
+};
 
 Todos.propTypes = {
-  todos: PropTypes.array
+  todos: PropTypes.array,
+  onCompleted: PropTypes.func,
+  onDeleted: PropTypes.func
 };
 
 export default Todos;
