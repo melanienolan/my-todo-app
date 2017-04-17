d5d08880c4d4d5be5290e2021faaf37df855e827
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-17 13:46:10 +0200

    Changed Title component to a functional component

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
-class Title extends Component {
-  render() {
-    let completed;
-    let total;
-    if (this.props.todos) {
-      total = this.props.todos.length;
-      completed = this.props.todos.filter(todo => todo.completed).length;
-    }
-    return (
-      <div className="Title">
-        <h1>My Todos</h1>
-        <p>{completed} / {total}</p>
-      </div>
-    );
+const Title = props => {
+  let completed;
+  let total;
+  if (props.todos) {
+    total = props.todos.length;
+    completed = props.todos.filter(todo => todo.completed).length;
   }
-}
+  return (
+    <div className="Title">
+      <h1>My Todos</h1>
+      <p>{completed} / {total}</p>
+    </div>
+  );
+};
 
 Title.propTypes = {
   todos: PropTypes.array
