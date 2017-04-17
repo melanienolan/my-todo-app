2a85bc14366b9a46492e5dca2dafa6a2650314ce
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-17 13:32:52 +0200

    Changed Input component to functional component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
-class Input extends Component {
-  onInputChange(value) {
-    this.props.onUpdate(value);
-  }
-  onAddTodo(value) {
-    this.props.onAddTodoSubmit(value);
-  }
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          value={this.props.inputValue}
-          onChange={event => this.onInputChange(event.target.value)}
-        />
-        <p>{this.props.inputValue}</p>
-        <button onClick={() => this.onAddTodo(this.props.inputValue)}>
-          Add todo
-        </button>
-      </div>
-    );
-  }
-}
+const Input = props => {
+  return (
+    <div>
+      <input
+        type="text"
+        value={props.inputValue}
+        onChange={event => props.onUpdate(event.target.value)}
+      />
+      <p>{props.inputValue}</p>
+      <button onClick={() => props.onAddTodoSubmit(props.inputValue)}>
+        Add todo
+      </button>
+    </div>
+  );
+};
 
 Input.propTypes = {
-  todo: PropTypes.object,
   inputValue: PropTypes.string,
   onAddTodoSubmit: PropTypes.func,
   onUpdate: PropTypes.func
