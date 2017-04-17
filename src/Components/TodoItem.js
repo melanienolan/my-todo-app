3eb14a22aea6b9d7d1d45a12fff5c1a57b355840
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-17 14:27:28 +0200

    Removed Component import statements from functional components

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+import React from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
 const TodoItem = props => {
