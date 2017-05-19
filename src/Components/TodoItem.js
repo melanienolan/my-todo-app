623f8aa6a80632508398245c7ee5e8d029b5feb7
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-05-19 14:39:12 +0200

    removed unused functions and changed icons

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,25 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Check from 'react-icons/lib/fa/check';
 import Close from 'react-icons/lib/fa/close';
-import SquareO from 'react-icons/lib/fa/square-o';
-import CheckSquareO from 'react-icons/lib/fa/check-square-o';
+
+import CircleThin from 'react-icons/lib/fa/circle-thin';
 
 const TodoItem = props => {
-  // function todoItemDone() {
-  //   todoItemStyles.backgroundColor = '#9ed1c7';
-  //   completedStyles.display = 'none';
-  // }
-  // function todoItemNotDone() {
-  //   todoItemStyles.backgroundColor = '#d19ea8';
-  // }
-  let completedClass = props.todo.completed ? 'completed' : 'notcompleted';
   return (
     <div className="todo-list__item">
       <button
         className="todo-list__button--completed"
         onClick={() => props.onCompleted(props.todo)}
       >
-        {props.todo.completed ? <CheckSquareO /> : <SquareO />}
+        {props.todo.completed ? <Check /> : <CircleThin />}
       </button>
       <p className="todo-list__text">
         {props.todo.title}
