8878b9135383951ee0269a9fc9161c3718de4b7c
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-05-19 12:16:30 +0200

    removed style variables

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,71 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Check from 'react-icons/lib/fa/check';
 import Close from 'react-icons/lib/fa/close';
+import SquareO from 'react-icons/lib/fa/square-o';
+import CheckSquareO from 'react-icons/lib/fa/check-square-o';
 
 const TodoItem = props => {
-  const todoItemStyles = {
-    backgroundColor: 'pink',
-    width: '400px',
-    margin: '10px auto',
-    borderRadius: '5px',
-    display: 'flex'
-  };
-  const pStyles = {
-    width: '300px',
-    padding: '10px',
-    color: 'white'
-  };
-  const buttonContainerStyles = {
-    width: '100px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  };
-  const buttonStyles = {
-    color: '#9e9e9e',
-    borderRadius: '50%',
-    border: 'none',
-    backgroundColor: '#ffffff',
-    margin: '2px',
-    width: '40px',
-    height: '40px'
-  };
-  const completedStyles = {
-    color: '#9e9e9e',
-    borderRadius: '50%',
-    border: 'none',
-    backgroundColor: '#ffffff',
-    margin: '2px',
-    width: '40px',
-    height: '40px'
-  };
-  function todoItemDone() {
-    todoItemStyles.backgroundColor = '#9ed1c7';
-    completedStyles.display = 'none';
-  }
-  function todoItemNotDone() {
-    todoItemStyles.backgroundColor = '#d19ea8';
-  }
-  props.todo.completed ? todoItemDone() : todoItemNotDone();
+  // function todoItemDone() {
+  //   todoItemStyles.backgroundColor = '#9ed1c7';
+  //   completedStyles.display = 'none';
+  // }
+  // function todoItemNotDone() {
+  //   todoItemStyles.backgroundColor = '#d19ea8';
+  // }
+  let completedClass = props.todo.completed ? 'completed' : 'notcompleted';
   return (
-    <div style={todoItemStyles}>
-      <p style={pStyles}>
+    <div className="todo-list__item">
+      <button
+        className="todo-list__button--completed"
+        onClick={() => props.onCompleted(props.todo)}
+      >
+        {props.todo.completed ? <CheckSquareO /> : <SquareO />}
+      </button>
+      <p className="todo-list__text">
         {props.todo.title}
       </p>
-      <div style={buttonContainerStyles}>
-        <button
-          style={completedStyles}
-          onClick={() => props.onCompleted(props.todo)}
-        >
-          <Check />
-        </button>
-        <button
-          style={buttonStyles}
-          onClick={() => props.onDeleted(props.todo)}
-        >
-          <Close />
-        </button>
-      </div>
+      <button
+        className="todo-list__button--delete"
+        onClick={() => props.onDeleted(props.todo)}
+      >
+        <Close />
+      </button>
     </div>
   );
 };
