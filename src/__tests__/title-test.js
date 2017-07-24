d39189a653679b0a7953c679ab6949dd3a9d2c00
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-24 13:30:36 +0200

    added snapshot for rendering of title component

--- src/__tests__/title-test.js
+++ src/__tests__/title-test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Title from '../Components/Title';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 it('renders without crashing', () => {
   shallow(<Title />);
 });
 
-it('renders title heading message', () => {
+it('renders title component', () => {
+  const wrapper = shallow(<Title />);
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('renders title header message', () => {
   const wrapper = shallow(<Title />);
   const welcome = <h1>My Todos</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
