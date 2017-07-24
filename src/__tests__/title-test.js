20a587a25a358a89d3a377924cbd958edcecce6d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-24 14:19:48 +0200

    added array of todos and updated test to check for correct number of complete and total todos

--- src/__tests__/title-test.js
+++ src/__tests__/title-test.js
@@ -3,12 +3,25 @@ import Title from '../Components/Title';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
+const todos = [
+  {
+    completed: false,
+    id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
+    title: 'hello'
+  },
+  {
+    completed: true,
+    id: '1c3db64b-fd95-4ea6-b438-fa1b90c1baeb',
+    title: 'completed todo'
+  }
+];
+
 it('renders without crashing', () => {
   shallow(<Title />);
 });
 
-it('renders title component', () => {
-  const wrapper = shallow(<Title />);
+it('renders correct number of complete and incomplete todos', () => {
+  const wrapper = shallow(<Title todos={todos} />);
   expect(toJson(wrapper)).toMatchSnapshot();
 });
 
