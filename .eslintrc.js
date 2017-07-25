22f4691278c8a7a20633b7cf7ae8c940d06c7f6b
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-25 10:56:20 +0200

    add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    'jest/globals': true
   },
   extends: ['eslint:recommended'],
   parserOptions: {
@@ -12,7 +13,7 @@ module.exports = {
     },
     sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['react', 'jest'],
   rules: {
     'no-unused-vars': 1,
     indent: ['error', 2],
@@ -21,6 +22,10 @@ module.exports = {
     semi: ['error', 'always'],
     'react/prop-types': 'error',
     'react/jsx-uses-vars': 2,
-    'react/jsx-uses-react': 2
+    'react/jsx-uses-react': 2,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/valid-expect': 'error'
   }
 };
