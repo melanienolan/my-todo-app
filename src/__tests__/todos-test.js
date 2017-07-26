ff35c60505eff28b33e9568a4c623153dffae54a
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-26 12:48:47 +0200

    moved todos array to separate file

--- src/__tests__/todos-test.js
+++ src/__tests__/todos-test.js
@@ -2,19 +2,7 @@ import React from 'react';
 import Todos from '../Components/Todos';
 import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-
-const todos = [
-  {
-    completed: true,
-    id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
-    title: 'complete todo'
-  },
-  {
-    completed: false,
-    id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
-    title: 'incomplete todo'
-  }
-];
+import { todos } from '../mocks/todos';
 
 it('renders todos component without crashing', () => {
   shallow(<Todos todos={todos} />);
---
+++ src/mocks/todos.js
@@ -0,0 +1,12 @@
+export const todos = [
+  {
+    completed: true,
+    id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
+    title: 'complete todo'
+  },
+  {
+    completed: false,
+    id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
+    title: 'incomplete todo'
+  }
+];
