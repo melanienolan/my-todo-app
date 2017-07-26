3261676f9e21f06489f362506e7eed7397363442
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-26 12:52:55 +0200

    imported todos array from mock data

--- src/__tests__/todoitem-test.js
+++ src/__tests__/todoitem-test.js
@@ -2,18 +2,10 @@ import React from 'react';
 import TodoItem from '../Components/TodoItem';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { todos } from '../mocks/todos';
 
-const completeTodo = {
-  completed: true,
-  id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
-  title: 'complete todo'
-};
-
-const incompleteTodo = {
-  completed: false,
-  id: 'e41fac97-3d2d-457a-9990-479f016ad0ce',
-  title: 'incomplete todo'
-};
+const completeTodo = todos[0];
+const incompleteTodo = todos[1];
 
 it('renders complete todo without crashing', () => {
   shallow(<TodoItem todo={completeTodo} />);
